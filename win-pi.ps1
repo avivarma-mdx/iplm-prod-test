8421640361b44a738089917e6428dc5baaf6a8be
Avi Varma <avi.varma@perforce.com>
2025-06-11 07:42:02 +0100

    init. all the things you need and currently works.

---
+++ win-pi.ps1
@@ -0,0 +1,13 @@
+$env:MDX_ROOT_DIR="C:\dev\tau"
+$env:PYTHONPATH="C:\dev\tau\src" # Not needed
+$env:MDX_CONFIG_DIR="C:\dev\tau\src\methodics\test\config"
+$env:MDX_PI_SERVER="http://localhost:8080"
+$env:MDX_VENV="C:\dev\tau\venv\dev" # Not needed
+$env:MDX_LICENSE_FILE="5289@flex.site.methodics-da.com"
+$env:MDX_PI_LOCALES_DIR="C:\dev\tau\locales"
+$env:P4PORT="12000"
+$env:MDX_PI_ASCII = 1
+$env:MDX_WORKSPACE_MANAGER = "PWM"
+$env:P4CONFIG=".p4config"
+$env:GIT_REPOSITORY_PATH=$env:MDX_ROOT_DIR
+$env:PRODUCT_PACKAGE="pi"
