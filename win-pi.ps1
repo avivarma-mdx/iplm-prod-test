d5795613e3b841566741943f464e2e3b38575efd
Avi Varma <avi.varma@perforce.com>
2025-06-11 07:59:15 +0100

    Updatd to work with the new install location.

--- win-pi.ps1
+++ win-pi.ps1
@@ -9,10 +9,10 @@ if (-not (Test-Path -Path $profilePath)) {
 # Append environment variables to the profile script
 @"
 `$env:MDX_ROOT_DIR="C:\dev\tau"
-`$env:MDX_CONFIG_DIR="C:\dev\tau\src\methodics\test\config"
+`$env:MDX_CONFIG_DIR="C:\Program Files\IPLM Client\config"
 `$env:MDX_PI_SERVER="http://localhost:8080"
 `$env:MDX_LICENSE_FILE="5289@flex.site.methodics-da.com"
-`$env:MDX_PI_LOCALES_DIR="C:\dev\tau\locales"
+`$env:MDX_PI_LOCALES_DIR="C:\Program Files\IPLM Client\locales"
 `$env:P4PORT="12000"
 `$env:MDX_PI_ASCII = 1
 `$env:MDX_WORKSPACE_MANAGER = "PWM"
